3f3855390ef403056b7c5187bb98cbe921200346
Sanne Crawford <s1184451@student.windesheim.nl>
2022-10-05 13:16:21 +0200

    IDK

--- week 5/week 5/6.11.cpp
+++ week 5/week 5/6.11.cpp
@@ -1,35 +1,18 @@
 #include<stdio.h>
 #define SIZE 10
 
-void sorting(unsigned int a[]);
-void smallest(unsigned const int a[]);
-
 int main(void) {
-	int d;
-	int x;
-	unsigned int g[]={0};
-	printf("Enter a number:");
-	scanf_s("%d", &d);
-	x = d + g;
-	sorting(g);
-	smallest(g);
-}
-void sorting(unsigned int a[]) {
-	for (unsigned int pass = 1; pass <= SIZE; ++pass) {
-		for (size_t j = 0; j <= SIZE; ++j) {
-			if (a[j] > a[j + 1]) {
-				unsigned int hold = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = hold;
+	int b[]{ 10 };
+	unsigned int rows[SIZE] = { 2,2,4,5,6,7,2,3,5 };
+
+	for (unsigned int pass = 1; pass < SIZE; ++pass) {
+		for (size_t j = 0; j < SIZE; ++j) {
+			if (b[j] > b[j + 1]) {
+				unsigned int hold = b[j];
+				b[j] = b[j + 1];
+				b[j + 1] = hold;
 			}
 		}
 	}
-}
-void smallest(unsigned const int a[]) {
-	for (size_t h = 0; h < SIZE; ++h) {
-		if (h % 20 == 0) {
-			puts("");
-		}
-		printf("%2u", a[h]);
-	}
+	printf("%u", rows);
 }
